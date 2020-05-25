706254a35a8525e7bb887cfd106f612f32339f47
Madel Liquido <madel.liquido@gmail.com>
2020-05-24 23:54:32 -0400

    Working on API calls

---
+++ client/build/precache-manifest.53bd606b0c09489b4a7778ebb079048d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bb2727bb4ac4cddb7d2a",
+    "url": "/static/js/main.883d832b.chunk.js"
+  },
+  {
+    "revision": "dde8759419eff0c6a9cb",
+    "url": "/static/js/2.ff7f80e9.chunk.js"
+  },
+  {
+    "revision": "bb2727bb4ac4cddb7d2a",
+    "url": "/static/css/main.68690ca2.chunk.css"
+  },
+  {
+    "revision": "6721641ab56a74e5f72bd76e73e61023",
+    "url": "/index.html"
+  }
+];
